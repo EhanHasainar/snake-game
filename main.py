821abb7009b8e893b99a24e1609cbb881bbbe5e0
Ehan Hasainar <ehanhasainar@gmail.com>
2025-05-06 13:42:26 +0400

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ while game_is_on:
         food.refresh()
         snake.extend()
         scoreboard.update_score()
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 260 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
     for segment in snake.segments:
